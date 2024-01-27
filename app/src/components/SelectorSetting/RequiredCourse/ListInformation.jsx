94e9f15846b26d729b6aa68daf11a6ac99b6053c
whats2000 <whats2000mc@gmail.com>
2024-01-27 17:22:15 +0800

    修正: 修正必修RWD手機佈局

--- app/src/components/SelectorSetting/RequiredCourse/ListInformation.jsx
+++ app/src/components/SelectorSetting/RequiredCourse/ListInformation.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Card, Button, Form, InputGroup} from "react-bootstrap";
+import {Card, Button, Form, InputGroup, Row, Col} from "react-bootstrap";
 import styled from "styled-components";
 import {websiteColor} from "../../../config";
 
@@ -71,46 +71,55 @@ class ListInformation extends Component {
 
         return (
             <Card.Body>
-                <ButtonsRow className="mb-2">
-                    <StyledSelect
-                        id="required-course-department"
-                        className="w-100"
-                        onChange={this.handleSelectDepartment}
-                        value={requiredCourseFilters['Department']}
-                    >
-                        <option value="">選擇系所</option>
-                        {filterOptions['系所'].options.map((option, index) => (
-                            <option key={index} value={option}>{option}</option>
-                        ))}
-                    </StyledSelect>
-                    <StyledSelect
-                        id="required-course-grade"
-                        onChange={this.handleSelectGrade}
-                        value={requiredCourseFilters['Grade']}
-                    >
-                        <option value="">選擇年級 (全)</option>
-                        {filterOptions['年級'].options.map((option, index) => (
-                            <option key={index} value={option}>{filterOptions['年級'].optionDisplayName[index]}</option>
-                        ))}
-                    </StyledSelect>
-                    <StyledSelect
-                        id="required-course-class"
-                        onChange={this.handleSelectClass}
-                        value={requiredCourseFilters['Class']}
-                    >
-                        <option value="">選擇班級 (全)</option>
-                        {filterOptions['班別'].options.map((option, index) => (
-                            <option key={index} value={option}>{option}</option>
-                        ))}
-                    </StyledSelect>
-                </ButtonsRow>
+                <Row>
+                    <Col lg={4} md={4} sm={12} xs={12} className="mb-2">
+                        <StyledSelect
+                            id="required-course-department"
+                            className="w-100"
+                            onChange={this.handleSelectDepartment}
+                            value={requiredCourseFilters['Department']}
+                        >
+                            <option value="">選擇系所</option>
+                            {filterOptions['系所'].options.map((option, index) => (
+                                <option key={index} value={option}>{option}</option>
+                            ))}
+                        </StyledSelect>
+                    </Col>
+                    <Col lg={4} md={4} sm={6} xs={6} className="mb-2">
+                        <StyledSelect
+                            id="required-course-grade"
+                            onChange={this.handleSelectGrade}
+                            value={requiredCourseFilters['Grade']}
+                            className="w-100"
+                        >
+                            <option value="">年級 (全)</option>
+                            {filterOptions['年級'].options.map((option, index) => (
+                                <option key={index}
+                                        value={option}>{filterOptions['年級'].optionDisplayName[index]}</option>
+                            ))}
+                        </StyledSelect>
+                    </Col>
+                    <Col lg={4} md={4} sm={6} xs={6} className="mb-2">
+                        <StyledSelect
+                            id="required-course-class"
+                            onChange={this.handleSelectClass}
+                            value={requiredCourseFilters['Class']}
+                            className="w-100"
+                        >
+                            <option value="">班級 (全)</option>
+                            {filterOptions['班別'].options.map((option, index) => (
+                                <option key={index} value={option}>{option}</option>
+                            ))}
+                        </StyledSelect>
+                    </Col>
+                </Row>
                 <ButtonsRow>
                     <InputGroup className="w-auto">
                         <InputGroup.Text>{totalCredits} 學分</InputGroup.Text>
                         <InputGroup.Text>{totalHours} 小時</InputGroup.Text>
                     </InputGroup>
-                    <StyledButton variant="success" onClick={this.handleSelectAll}>全部填入</StyledButton>
-                    <Button variant="danger" className="ms-2" onClick={this.handleDeselectAll}>全部取消</Button>
+                    <StyledButton variant="success" onClick={this.handleSelectAll}>全填</StyledButton>
+                    <Button variant="danger" className="ms-2" onClick={this.handleDeselectAll}>全取消</Button>
                 </ButtonsRow>
             </Card.Body>
         );
